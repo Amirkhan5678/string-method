b54dd35a906c0dc10ce221444d521bb003960f1a
sh4444qtr@gmail.com <sh4444qtr@gmail.com>
2022-04-19 05:10:30 +0500

    string method is complete

---
+++ app.js
@@ -0,0 +1,144 @@
+// var string=("assignment 21-25");
+// document.write(string);
+
+// question no: 01//
+
+// var firstName = prompt("");
+// var lastName = prompt("");
+// var result = (firstName+lastName);
+// alert(result);
+
+// question no: 02//
+
+// var userInput = prompt("");
+// // alert(userInput.length);
+//  document.write("my favorite phone is  " + userInput +"<br />"+"Lenght of string is  " + userInput.length);
+
+// question no: 03//
+
+// var string = prompt("");
+// var indexNum =string.indexOf("n");
+// // alert(string)
+// // alert(indexNum);
+// document.write("string : "+string+"<br />" + "index number : " +indexNum);    
+
+// question no: 04//
+
+
+// var string = ("hello world");
+// var indexNum =string.lastIndexOf("l");
+// // alert(string)
+// // alert(indexNum);
+// document.write("string : "+string+"<br />" + "index number : " +indexNum);    
+
+
+// question no: 05//
+
+// var string = ("pakistani");
+// var indexNum =string.charAt(3);
+// document.write("string : "+string+"<br />" + "character at index : " +indexNum);    
+
+// question no: 06//
+
+// var string1 = prompt("");
+// var string2 = prompt("");
+// var result = result.concat(string1,string2);
+// alert(result);  
+
+// question no: 07//
+
+// var text = ("hyderabad");
+// var newText = text.replace("hyder","Islam");
+// // alert(newText);
+// document.write("city : "+ text +"<br />"+"After replacement : " +newText);
+
+// question no: 08//
+
+// var message = ("“Ali and Sami are best friends. They play cricket and football together.”");
+// var newText = message.replace(/and/g,"&");    
+// document.write("Old message : " +message+"<br />"+ "new message : "+newText);
+
+// question no: 09//
+
+// var value = ("472");
+// var num =value.replace("472",472);
+// document.write("value : "+value+"<br />"+"type : string "+"<br />"+ "value : " +num+ "<br />"+"type : num "); 
+
+// question no: 10//
+
+// var userInput = prompt("");
+// var up = userInput.toUpperCase("");
+// console.log(up);
+
+// question no: 11//
+
+// var userInput = prompt("");
+// var up = userInput.charAt(0).toUpperCase() + userInput.slice(1)
+// document.write(up)
+
+
+// question no: 12//
+
+// var value = (35.36);
+// var num =value.toString();
+// alert(num);
+
+// question no: 13//
+
+// var text = prompt("");
+// for(var i = text; i=text.length; i++){
+
+// if(text ==="@"){
+//     alert("welcome!");}
+// else{
+//     alert("invalid username ")
+// }
+// }
+
+ // question no: 14//
+
+//  var arr = ["cake","patties","applecake","cookies"];
+// var input = prompt("");
+// for(var arr = i; i=arr.length; i++){
+
+// if (arr === input) {
+//     alert("welcome to our backery!");
+// }
+// else {
+//     alert("sorry");
+// }
+// }
+   
+// question no: 15//
+
+// var password = prompt("");
+// for(var i = password; i=password.length; i++){
+//     if(password === "alphabet && Number"){
+//         console.log("welcome!")
+//     }
+//     else{ console.log("sorry please try again")}
+// }
+
+// question no: 16//
+
+
+// for(var i = "[U N I V E R S I T Y O F K A R A C H I]";i= i;i++){
+// document.write(i);
+// }
+
+// question no: 17//
+
+// var text = ("pakistan");
+// var txt = text.slice(-1);
+// document.write("user input is " + text + "<br />" + "last character is " + txt)  
+
+// question no: 18//
+
+// var str = "The quick brown fox jumps over the lazy dog”. Write a program to count number of occurrences of word “the” in given string",
+// count = 0;
+// for(var i = 0; i<str.length; i++){
+//     if(str.charAt(i)==="the"){
+//        count+= 1 
+//     }
+// }
+// document.write(count);
